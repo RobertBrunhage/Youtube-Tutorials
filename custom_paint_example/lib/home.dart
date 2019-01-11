f479fb676733ada938b2716d77ffd4acf1cb8f45
Robert Brunhage <Robert.brunhage@ascenda.se>
2019-01-11 16:24:15 +0100

    custom paint example

---
+++ custom_paint_example/lib/home.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomPaint(
+        painter: BluePainter(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class BluePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final height = size.height;
+    final width = size.width;
+    Paint paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, width, height));
+    paint.color = Colors.blue.shade700;
+    canvas.drawPath(mainBackground, paint);
+
+    Path ovalPath = Path();
+    // Start paint from 20% height to the left
+    ovalPath.moveTo(0, height * 0.2);
+
+    // paint a curve from current position to middle of the screen
+    ovalPath.quadraticBezierTo(
+        width * 0.45, height * 0.25, width * 0.51, height * 0.5);
+
+    // Paint a curve from current position to bottom left of screen at width * 0.1
+    ovalPath.quadraticBezierTo(width * 0.58, height * 0.8, width * 0.1, height);
+
+    // draw remaining line to bottom left side
+    ovalPath.lineTo(0, height);
+
+    // Close line to reset it back
+    ovalPath.close();
+
+    paint.color = Colors.blue.shade600;
+    canvas.drawPath(ovalPath, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
---
+++ custom_paint_example/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:custom_paint_example/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
