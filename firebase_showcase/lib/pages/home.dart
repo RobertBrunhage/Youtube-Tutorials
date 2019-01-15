362d4a48315ff7b4be7f9dfbe377ffd2e0c8013d
Robert Brunhage <Robert.brunhage@ascenda.se>
2019-01-15 08:47:42 +0100

    fix to firebase example

--- firebase_showcase/lib/pages/home.dart
+++ firebase_showcase/lib/pages/home.dart
@@ -2,11 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 class Home extends StatelessWidget {
   const Home({Key key, this.user}) : super(key: key);
   final FirebaseUser user;
-
 
   @override
   Widget build(BuildContext context) {
@@ -15,34 +13,42 @@ class Home extends StatelessWidget {
         title: Text('Home ${user.email}'),
       ),
       body: StreamBuilder<DocumentSnapshot>(
-        stream: Firestore.instance.collection('users').document(user.uid).snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot){
-          if(snapshot.hasError){
+        stream: Firestore.instance
+            .collection('users')
+            .document(user.uid)
+            .snapshots(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            return checkRole(snapshot.data);
           }
-          switch(snapshot.connectionState){
-            case ConnectionState.waiting: return Text('Loading..');
-            default:
-              return checkRole(snapshot.data);
-          }
+          return LinearProgressIndicator();
         },
       ),
     );
   }
 
-  Center checkRole(DocumentSnapshot snapshot){
-    if(snapshot.data['role'] == 'admin'){
+  Center checkRole(DocumentSnapshot snapshot) {
+    if (snapshot.data == null) {
+      return Center(
+        child: Text('no data set in the userId document in firestore'),
+      );
+    }
+    if (snapshot.data['role'] == 'admin') {
       return adminPage(snapshot);
-    }else{
+    } else {
       return userPage(snapshot);
     }
   }
 
-  Center adminPage(DocumentSnapshot snapshot){
-    return Center(child: Text('${snapshot.data['role']} ${snapshot.data['name']}'));
+  Center adminPage(DocumentSnapshot snapshot) {
+    return Center(
+        child: Text('${snapshot.data['role']} ${snapshot.data['name']}'));
   }
 
-  Center userPage(DocumentSnapshot snapshot){
+  Center userPage(DocumentSnapshot snapshot) {
     return Center(child: Text(snapshot.data['name']));
   }
 }
