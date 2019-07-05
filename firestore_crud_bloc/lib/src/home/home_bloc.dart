7ce89aa392145b71f6a94f995808bdbb75a5d1ac
Robert Brunhage <Robert.brunhage@ascenda.se>
2019-07-05 08:35:24 +0200

    fixed the stream issue by adding a BehaviorSubject. Refer to https://github.com/RobertBrunhage/Youtube-Tutorials/issues/3

--- firestore_crud_bloc/lib/src/home/home_bloc.dart
+++ firestore_crud_bloc/lib/src/home/home_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firestore_crud/bloc_provider.dart';
 import 'package:firestore_crud/repository/db.dart';
+import 'package:rxdart/rxdart.dart';
 
 class HomeBloc implements BlocBase {
   HomeBloc() {
@@ -10,11 +11,13 @@ class HomeBloc implements BlocBase {
   }
 
   String id;
-  final _idController = StreamController<String>();
+
+  /// Refer to https://github.com/RobertBrunhage/Youtube-Tutorials/issues/3 to see why we use BehaviorSubject instead.
+  final _idController = BehaviorSubject<String>();
   Stream<String> get outId => _idController.stream;
   Sink<String> get _inId => _idController.sink;
 
-  final _firestoreController = StreamController<QuerySnapshot>();
+  final _firestoreController = BehaviorSubject<QuerySnapshot>();
   Stream<QuerySnapshot> get outFirestore => _firestoreController.stream;
   Sink<QuerySnapshot> get _inFirestore => _firestoreController.sink;
 
